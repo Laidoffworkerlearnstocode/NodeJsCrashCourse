8d7a0df5fca8cfedc0cc67bf28bb44296f93ead9
laidoffworkerlearnstocode <kwg6qlyv@hotmail.com>
2023-04-27 23:05:43 +0800

    20230427

--- fsModules.js
+++ fsModules.js
@@ -1,14 +1,17 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 const filePath1 = path.resolve(__dirname, 'content', '2nd.txt');
+data = '\nThis is the new data';
 
 
 
-fs.readFile(filePath1, 'utf8', (err, result) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log(result);
-});
+async function readAndWrite(_filePath) {
+    const text = await fs.readFile(_filePath, 'utf8');
+    console.log(text);
+    await fs.writeFile(_filePath, data , {flag: 'a'});
+    const newText = await fs.readFile(_filePath, 'utf8');
+    console.log(newText);
+}
+
+readAndWrite(filePath1);
